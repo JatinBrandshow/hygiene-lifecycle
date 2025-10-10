8733fcaafddf0ffa09b41b753a38ef568759e036
JatinBrandshow <jatin.yadav@brandshow.in>
2025-10-10 13:37:19 +0530

    New Changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,7 +15,7 @@ const Footer = () => {
     ];
 
     const headOfficeLocation = "Plot No - 736-C ,sector 37 , pace city 2, Gurugram, Haryana 122001";
-    const manufacturingPlantLocation = "1st Floor, Khasra No, 2011/471, Bhatolikalan Solan, (H.P)";
+    const manufacturingPlantLocation = "1st Floor, Khasra No, 2011/471, Bhatoli Kalan Solan, (H.P)";
 
     return (
         <footer className="relative bg-gradient-tertiary text-white overflow-hidden">
